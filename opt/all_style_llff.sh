3fb080af3c42c3761150f3073ac08e722c8d50d5
koolo233 <koolo233@163.com>
2023-09-27 21:10:15 +0800

    bug fixes: error of scenes' name

--- opt/all_style_llff.sh
+++ opt/all_style_llff.sh
@@ -23,23 +23,25 @@ done
 
 datasets=${DATASET:-"trex horns flower fern orchids fortress leaves room"}
 
-for dataset in ${datasets}
+for SCENE in ${datasets}
 do
-  for ((style_id=1; style_id<=140; style_id++))
+  for ((STYLE=0; STYLE<=140; STYLE++))
   do
 
     echo "-----------------------------------------------"
-    echo "Style_id: ${style_id}"
-    echo "dataset: ${dataset}"
+    echo "Style_id: ${STYLE}"
+    echo "dataset: ${SCENE}"
     echo "Begin training of NeRF..."
 
-    ckpt_svox2=ckpt_svox2/llff/${SCENE}
-    ckpt_arf=ckpt_arf/llff/${SCENE}_${style_id}
-    data_dir=../data/llff/${SCENE}
-    style_img=../data/styles/${style_id}.jpg
+    data_type=llff
+    ckpt_svox2=ckpt_svox2/${data_type}/${SCENE}
+    ckpt_arf=ckpt_arf/${data_type}/${SCENE}_${STYLE}
+    data_dir=../data/${data_type}/${SCENE}
+    style_img=../data/styles/${STYLE}.jpg
 
     if [[ ! -f "${ckpt_svox2}/ckpt.npz" ]]; then
-        CUDA_VISIBLE_DEVICES=${GPU_ID} python opt.py -t ${ckpt_svox2} ${data_dir} -c configs/llff.json
+        CUDA_VISIBLE_DEVICES=${GPU_ID} python opt.py -t ${ckpt_svox2} ${data_dir} \
+                        -c configs/llff.json
     fi
 
     if [[ ! -f "${ckpt_arf}/ckpt.npz" ]]; then
@@ -51,7 +53,7 @@ do
                     --content_weight 1e-3
     fi
 
-    python render_imgs.py ${ckpt_arf}/ckpt.npz ${data_dir} --render_path
+    CUDA_VISIBLE_DEVICES=${GPU_ID} python render_imgs.py ${ckpt_arf}/ckpt.npz ${data_dir} --render_path
 
     echo "Done training of NeRF."
     echo "-----------------------------------------------"
